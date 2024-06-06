c10a72a6e0d1d106fef9dde79eb5eede36451dfb
Layrton Silva <98564718+layrtonz@users.noreply.github.com>
2024-06-05 23:49:41 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    {date: new Date(2024, 5, 5), category: 'food', title: 'McDonalds', value: 37.50},
+    {date: new Date(2024, 5, 10), category: 'food', title: 'Burguer King', value: 55.50},
+    {date: new Date(2024, 5, 16), category: 'rent', title: 'Aluguel', value: 1700},
+    {date: new Date(2024, 6, 12), category: 'salary', title: 'Salário Min', value: 4200},
+];
