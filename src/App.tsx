2432fb72a63c3124f8ad4645b3f2a7d20947c17c
Layrton Silva <98564718+layrtonz@users.noreply.github.com>
2024-06-06 21:31:32 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { Item } from './types/Item';
 import { categories } from './data/categories'
 import { items } from './data/items'
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
-
+import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -17,19 +18,29 @@ const App = () => {
 
   }, [list, currentMonth]);
 
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText> Sistema Financeiro </C.HeaderText>
       </C.Header>
       <C.Body>
-        {/* Área de informações */}
+
+        <InfoArea 
+        currentMonth={currentMonth}
+        onMonthChange={handleMonthChange}
+        />
 
         
         {/* Área de Inserção */}
         
         
         {/* Tabela de Itens */}
+        <TableArea list={filteredList} />
       </C.Body>
     </C.Container>
   );
